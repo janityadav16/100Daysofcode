0e08e06fd2d7cbfd638bfb7b27373d2a8c9553b9
Janit Yadav <janityadav16@gmail.com>
2023-07-07 09:53:40 +0530

    to learn

--- javascript/array.js
+++ javascript/array.js
@@ -1,4 +1,4 @@
-          
+  
  let x =[ 23,"fsyuuas",true,43]
 
  for(let i = 0; i <= 3; i++){
@@ -6,3 +6,19 @@
  }
 
  console.log(x.length)
+
+
+ function x(z) {
+    return z * 5
+}
+ console.log("----->>>",z)
+
+
+ sum(3,x)
+
+ let multiply3 = val =>{
+     return val*3
+}
+(val => {
+     console.log("sgdjsdg") 
+    })()

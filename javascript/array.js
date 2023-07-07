6a8dee9452d0a0565f2de0c3a7341bb2ee2849ac
Janit Yadav <janityadav16@gmail.com>
2023-07-07 08:54:03 +0530

    to learn

--- javascript/array.js
+++ javascript/array.js
@@ -1,61 +1,8 @@
-//        //0,   1,       2,   3  
-// // let x =[ 23,"fsyuuas",true,43]
+          
+ let x =[ 23,"fsyuuas",true,43]
 
-// // for(let i = 0; i <= 3; i++){
-// // console.log(x[i])
-// // }
+ for(let i = 0; i <= 3; i++){
+ console.log(x[i])
+ }
 
-// // console.log(x.length)
-
-// //array properties
-// //length
-// //map,filter reduce,forEach
-// //array push ,pop,shift ,unshift
-// ////slice,splice, join
-
-
-// // function length(){
-
-// // }
-
-// // let sum = function(){
-
-// // }
-
-// // let sum = () => {
-
-// // }
-
-// let sum = (a,b) => {
-//     // console.log(a,b)
-//     a*=3;
-//     console.log(a)
-//     b(a);
-// }
-
-// let x = z => z*5
-//     // console.log("----->>>",z)
-
-
-// sum(3,x)
-
-// // let multiply3 = val =>{
-// //     return val*3
-// // }
-
-// // //Anonymous function //IIFE
-// // (val => {
-// //     console.log("sgdjsdg")
-// // })()
-
-
-
-
-let arr=[3,4,6,7,8]
-// let x = ()=>{console.log("ejgdj")}
-arr.forEach(element=>console.log(element))
-let y =arr.map(element=>console.log(element))
-console.log
-// let x = arr.pop()
-// console.log(arr,x)
-// console.log(arr.indexOf(7))
+ console.log(x.length)

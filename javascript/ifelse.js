46e4261c0159aa3ec59889d6dbdb1beadf62a5e9
Janit Yadav <janityadav16@gmail.com>
2023-07-08 21:02:27 +0530

    to practice

--- javascript/ifelse.js
+++ javascript/ifelse.js
@@ -34,3 +34,23 @@ else{
      console.log(D)
  }
  
+
+
+
+
+  let a=90-100
+ if(a=90-100){
+     console.log(A)
+ }
+ let a=80-90
+ if(a=80-90){
+     console.log(B)
+ }
+ let a=70-80
+ if(a=70-80){
+     console.log(C)
+ }
+ let a=0-70
+ if(a=0-70){
+     console.log(D)
+ }

12829c1aca8454f72a255cea6b899b472dc1e2a9
Janit Yadav <janityadav16@gmail.com>
2023-07-08 12:52:32 +0530

    to practice

--- javascript/function.js
+++ javascript/function.js
@@ -22,6 +22,30 @@ function calculatePercentage(subject1, subject2, subject3, subject4, subject5) {
   );
   
   console.log("Total Percentage: " + result + "%");
+
+
+
+  function add(a,b){
+    return a+b
+}
+
+add(4,5)
+
+
+function calculateGrade(score) {
+    if (score >= 90) {
+      return 'A';
+    } else if (score >= 80) {
+      return 'B';
+    } else if (score >= 70) {
+      return 'C';
+    } else if (score >= 60) {
+      return 'D';
+    } else {
+      return 'F';
+    }
+  }
+  console.log(calculateGrade(55))
   
 
 

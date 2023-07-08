7c1d8f6e731deb09c7eab73f68b61a37126dc1cb
Janit Yadav <janityadav16@gmail.com>
2023-07-08 21:59:30 +0530

    to practice

---
+++ server.js
@@ -0,0 +1,31 @@
+
+// //for, while, do while
+ 
+
+// //for
+// for(let i=1; i<=50; i++){
+//     console.log(i)
+// }
+
+// //while
+// let x =1;
+// while(x<=50){
+//     console.log("while",x)
+//     x++;
+// }
+
+// //do while
+// let y=1
+// do{
+//     console.log("do-while",y)
+//     y++;
+// }while(y<=50)
+
+function grade(A){
+        let x=90-100;
+        if(90-100){
+            console.log(A)
+        }
+
+}
+grade(78)

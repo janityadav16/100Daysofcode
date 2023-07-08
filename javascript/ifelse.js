4955b422c5981686b0b3caefabc37a14e54cc61f
Janit Yadav <janityadav16@gmail.com>
2023-07-08 18:27:25 +0530

    to learn

--- javascript/ifelse.js
+++ javascript/ifelse.js
@@ -14,3 +14,22 @@ else if(a>0 && a<70){
 else{
     console.log("wrong marks")
 }
+
+
+
+ let a=90-100
+ if(a=90-100){
+     console.log(A)
+ }
+ let a=80-90
+ if(a=80-90){
+     console.log(B)
+ }
+  let a=70-80
+ if(a=70-80){
+     console.log(C)
+ }
+ let a=0-70
+ if(a=0-70){
+     console.log(D)
+ }

b4f9b3e2e4ef6e23d3debe4e7b83159a96157f2c
Janit Yadav <janityadav16@gmail.com>
2023-07-10 21:20:29 +0530

    to learn

--- javascript/server.js
+++ javascript/server.js
@@ -3,23 +3,23 @@
  
 
 // //for
-// for(let i=1; i<=50; i++){
-//     console.log(i)
-// }
+ for(let i=1; i<=50; i++){
+     console.log(i)
+ }
 
 // //while
-// let x =1;
-// while(x<=50){
-//     console.log("while",x)
-//     x++;
-// }
+ let x =1;
+ while(x<=50){
+     console.log("while",x)
+     x++;
+  }
 
-// //do while
-// let y=1
-// do{
-//     console.log("do-while",y)
-//     y++;
-// }while(y<=50)
+ //do while
+ let y=1
+ do{
+     console.log("do-while",y)
+     y++;
+}while(y<=50)
 
 function grade(A){
         let x=90-100;

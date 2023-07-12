5077b257bcf28ef53547754a41ee3df8a264786d
Janit Yadav <janityadav16@gmail.com>
2023-07-12 16:35:48 +0530

    to learn

--- javascript/variable.js
+++ javascript/variable.js
@@ -1,43 +1,49 @@
-const totalNumber=200
+const totalNumber = 200;
+const CITY = "alwar";
 
-function studentdetail(name, age, city, qualify,marks){
-    console.log("Student Name: "+ name)
-    console.log("Student Age: "+ age)
-    console.log("City: "+ city)
-    console.log("Qualify: "+ qualify)
-    console.log("marks: P("+ marks[0]+"), C("+ marks[1]+"), M("+ marks[2]+")")
-    let percent=percentage(marks, totalNumber)
-    console.log("percentage: "+ percent+ "%")
-    console.log("Grade: "+ grade(percent))
-}
+const studentdetail = (name, age, city, qualify, marks) => {
+  console.log(`Student Name: ${name}`);
+  console.log(`Student Age: ${age}`);
+  console.log(`City: ${city}`);
+  console.log(`Qualify: ${qualify}`);
+  console.log(`Marks: P(${marks[0]}), C(${marks[1]}), M(${marks[2]})`);
 
- function percentage(marks, totalCredits){
-  //marks obtained/totalCredits*100
-  let obtainedMarks=marks[0]+ marks[1]+ marks[2]
-  let percentage=(obtainedMarks*100)/totalCredits
-  return percentage
- }
- function grade(percent){
-   if(percent>=90){
-    return "A+"
-   }else if(percent>=80 && percent<=90){
-      return "A"
-  }else if(percent>=70 && percent<=80){
-    return "B"
-  }else{
-     return "FAIL"
+  let percent = percentage(marks, totalNumber);
+  console.log(`Percentage: ${percent}%`);
+  console.log(`Grade: ${grade(percent)}`);
+};
+
+const percentage = (marks, totalCredits) => {
+  let obtainedMarks = marks[0] + marks[1] + marks[2];
+  let percentage = (obtainedMarks * 100) / totalCredits;
+  return percentage;
+};
+
+const grade = (percent) => {
+  if (percent >= 90) {
+    return "A+";
+  } else if (percent >= 80 && percent <= 90) {
+    return "A";
+  } else if (percent >= 70 && percent <= 80) {
+    return "B";
+  } else {
+    return "FAIL";
   }
- }
-const CITY="alwar"
+};
 
+const studentName = ["janit", "Nakul", "vandana"];
+const studentAge = [18, 19, 44];
+const qualify = [true, false, true];
+const marks = [
+  [55, 44, 66],
+  [50, 46, 56],
+  [53, 54, 66],
+];
 
-let studentName =["janit", "Nakul", "vandana"]
-var studentAge=[18, 19, 44]
-var qualify=[true, false, true]
-let marks=[[55,44,66], [50,46,56], [53,54,66]]
-for(i=0; i<3; i++){
-    studentdetail(studentName[i], studentAge[i], CITY, qualify[i],marks[i])
+for (let i = 0; i < 3; i++) {
+  studentdetail(studentName[i], studentAge[i], CITY, qualify[i], marks[i]);
 }
+
 
 
 function calculator(n, m, operation){

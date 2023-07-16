c79e47e57ce7af7174c49063355fdcc2663574ae
Janit Yadav <janityadav16@gmail.com>
2023-07-16 21:17:25 +0530

    to learn

--- javascript/array.js
+++ javascript/array.js
@@ -1,28 +1,36 @@
   
- let x =[ 23,"fsyuuas",true,43]
+//  let x =[ 23,"fsyuuas",true,43]
 
- for(let i = 0; i <= 3; i++){
- console.log(x[i])
- }
+//  for(let i = 0; i <= 3; i++){
+//  console.log(x[i])
+//  }
 
- console.log(x.length)
+//  console.log(x.length)
 
 
- function x(z) {
-    return z * 5
+//  function x(z) {
+//     return z * 5
+// }
+//  console.log("----->>>",z)
+
+
+//  sum(3,x)
+
+//  let multiply3 = val =>{
+//      return val*3
+// }
+// (val => {
+//      console.log("sgdjsdg") 
+//     })()
+
+
+//     const fruits = ["apple", "banana", "orange", "grape", "mango"];
+// console.log(fruits);
+
+
+const myArray = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
 }
- console.log("----->>>",z)
 
-
- sum(3,x)
-
- let multiply3 = val =>{
-     return val*3
-}
-(val => {
-     console.log("sgdjsdg") 
-    })()
-
-
-    const fruits = ["apple", "banana", "orange", "grape", "mango"];
-console.log(fruits);

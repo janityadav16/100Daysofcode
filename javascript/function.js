2f5210e959fe0de1109eb854ef9a8fbb931c662e
Janit Yadav <janityadav16@gmail.com>
2023-07-16 21:14:09 +0530

    to learn

--- javascript/function.js
+++ javascript/function.js
@@ -1,65 +1,72 @@
 
-function calculatePercentage(subject1, subject2, subject3, subject4, subject5) {
-    const totalMarks = subject1 + subject2 + subject3 + subject4 + subject5;
-    const totalSubjects = 5;
-    const percentage = (totalMarks / (totalSubjects * 100)) * 100;
+// function calculatePercentage(subject1, subject2, subject3, subject4, subject5) {
+//     const totalMarks = subject1 + subject2 + subject3 + subject4 + subject5;
+//     const totalSubjects = 5;
+//     const percentage = (totalMarks / (totalSubjects * 100)) * 100;
   
-    return percentage;
-  }
+//     return percentage;
+//   }
 
-  const subject1Marks = 80;
-  const subject2Marks = 90;
-  const subject3Marks = 85;
-  const subject4Marks = 75;
-  const subject5Marks = 95;
+//   const subject1Marks = 80;
+//   const subject2Marks = 90;
+//   const subject3Marks = 85;
+//   const subject4Marks = 75;
+//   const subject5Marks = 95;
   
-  const result = calculatePercentage(
-    subject1Marks,
-    subject2Marks,
-    subject3Marks,
-    subject4Marks,
-    subject5Marks
-  );
+//   const result = calculatePercentage(
+//     subject1Marks,
+//     subject2Marks,
+//     subject3Marks,
+//     subject4Marks,
+//     subject5Marks
+//   );
   
-  console.log("Total Percentage: " + result + "%");
+//   console.log("Total Percentage: " + result + "%");
 
 
 
-  function add(a,b){
-    return a+b
+//   function add(a,b){
+//     return a+b
+// }
+
+// add(4,5)
+
+
+// function calculateGrade(score) {
+//     if (score >= 90) {
+//       return 'A';
+//     } else if (score >= 80) {
+//       return 'B';
+//     } else if (score >= 70) {
+//       return 'C';
+//     } else if (score >= 60) {
+//       return 'D';
+//     } else {
+//       return 'F';
+//     }
+//   }
+//   console.log(calculateGrade(55))
+
+  
+// function calculateAveragePercentage(marks) {
+//   let totalMarks = 0;
+
+//   for (let i = 0; i < marks.length; i++) {
+//     totalMarks += marks[i];
+//   }
+
+//   const averagePercentage = totalMarks / marks.length;
+
+//   return averagePercentage;
+// }
+
+// const studentMarks = [85, 90, 78, 92, 88];
+// const average = calculateAveragePercentage(studentMarks);
+// console.log("Average Percentage:", average);
+
+function addNumbers(num1, num2) {
+  return num1 + num2;
 }
 
-add(4,5)
-
-
-function calculateGrade(score) {
-    if (score >= 90) {
-      return 'A';
-    } else if (score >= 80) {
-      return 'B';
-    } else if (score >= 70) {
-      return 'C';
-    } else if (score >= 60) {
-      return 'D';
-    } else {
-      return 'F';
-    }
-  }
-  console.log(calculateGrade(55))
-
-  
-function calculateAveragePercentage(marks) {
-  let totalMarks = 0;
-
-  for (let i = 0; i < marks.length; i++) {
-    totalMarks += marks[i];
-  }
-
-  const averagePercentage = totalMarks / marks.length;
-
-  return averagePercentage;
-}
-
-const studentMarks = [85, 90, 78, 92, 88];
-const average = calculateAveragePercentage(studentMarks);
-console.log("Average Percentage:", average);
+const result = addNumbers(5, 7);
+console.log(result); 
